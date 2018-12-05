1123aef95d6316e934f0baf9df49c3ca6e778729
user <arianaariska@gmail.com>
2018-12-05 13:54:17 +0200

    20181205_13_53

---
+++ dgr_20181205.py
@@ -0,0 +1,141 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> from math importn sin as bad_sinus
+SyntaxError: invalid syntax
+>>> from math import sin as bad_sinus
+>>> x{0}
+SyntaxError: invalid syntax
+>>> x[0]
+
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    x[0]
+NameError: name 'x' is not defined
+>>> type(x)
+
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    type(x)
+NameError: name 'x' is not defined
+>>> x
+
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    x
+NameError: name 'x' is not defined
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 14, in <module>
+    from matplotlib import pyploy as plt
+ImportError: cannot import name pyploy
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+
+Warning (from warnings module):
+  File "/usr/lib/python2.7/dist-packages/matplotlib/font_manager.py", line 273
+    warnings.warn('Matplotlib is building the font cache using fc-list. This may take a moment.')
+UserWarning: Matplotlib is building the font cache using fc-list. This may take a moment.
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 17, in <module>
+    ply.xlabel("x")
+NameError: name 'ply' is not defined
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 17, in <module>
+    ply.xlabel("x")
+NameError: name 'ply' is not defined
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 17, in <module>
+    ply.xlabel("x")
+NameError: name 'ply' is not defined
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 17, in <module>
+    ply.xlabel("x")
+NameError: name 'ply' is not defined
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 17, in <module>
+    ply.xlabel("x")
+NameError: name 'ply' is not defined
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 17, in <module>
+    ply.xlabel("x")
+NameError: name 'ply' is not defined
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+['$sin(x)$ - default - viss ir savienots ar taisnaam liinijaam']
+['$sin(x)$ - default - viss ir savienots ar taisnaam liinijaam', '$sin(x)$ - tikai dazhi punkti']
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+['$sin(x)$ - default - viss ir savienots ar taisnaam liinijaam']
+['$sin(x)$ - default - viss ir savienots ar taisnaam liinijaam', '$sin(x)$ - tikai dazhi punkti']
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 40, in <module>
+    derivative.append(temp)
+NameError: name 'derivative' is not defined
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+
+Traceback (most recent call last):
+  File "/home/user/atvasinajumi.py", line 72, in <module>
+    plt.legend(legend, loc = 3, fancybox=True, framealpha=0.01, facecolor="green")
+  File "/usr/lib/python2.7/dist-packages/matplotlib/pyplot.py", line 3553, in legend
+    ret = gca().legend(*args, **kwargs)
+  File "/usr/lib/python2.7/dist-packages/matplotlib/axes/_axes.py", line 538, in legend
+    self.legend_ = mlegend.Legend(self, handles, labels, **kwargs)
+TypeError: __init__() got an unexpected keyword argument 'facecolor'
+>>> 
+==================== RESTART: /home/user/atvasinajumi.py ====================
+[]
+>>> 
+============== RESTART: /home/user/Datormacibas/atvasinajumi.py ==============
+[]
